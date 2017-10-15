1f4a80fb53a4f822dcf381326a1a978a8490ff2d
liujiachen <liujiachen@baidu.com>
2017-10-15 10:11:37 +0800

    simple demo ready

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# File Name: build.sh
+# @Author: caoying - liujiachen@baidu.com
+# Created Time : Sun Oct 15 10:08:59 2017
+# Description:
+
+g++ -I ./eigen/ demo_simple.cpp -o demo_simple
